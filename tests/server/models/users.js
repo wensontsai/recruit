2889aeb0d17e8cef618e655734c718bd647ccf5c
Wenson Tsai <wensontsai@gmail.com>
2016-03-22 16:06:37 -0400

    models

--- tests/server/models/users.js
+++ tests/server/models/users.js
@@ -11,38 +11,14 @@ describe('User Model', () => {
     // run user Create func
 
   });
-  it('has an initial state', () => {
-    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
-    const nextState = reducer(undefined, action);
-    expect(nextState).to.equal(fromJS({
-      entries: ['Trainspotting']
-    }));
+  it('can get all users', () => {
+    // query DB
+    //  
+
   });
+  it('can remove a user', () => {
+    // query DB
+    //  
 
-  it('handles SET_ENTRIES', () => {
-    const initialState = Map();
-    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
-    const nextState = reducer(initialState, action);
-
-    expect(nextState).to.equal(fromJS({
-      entries: ['Trainspotting']
-    }));
   });
-
-  it('handles NEXT', () => {
-    const initialState = fromJS({
-      entries: ['Trainspotting', '28 Days Later']
-    });
-    const action = {type: 'NEXT'};
-    const nextState = reducer(initialState, action);
-
-    expect(nextState).to.equal(fromJS({
-      vote: {
-        pair: ['Trainspotting', '28 Days Later']
-      },
-      entries: []
-    }));
-  });
-  
-
 });
